095cba58ab4f2a15cf09aa1af55a61349b176167
Jeffried-Dev <nouadjenouleho@gmail.com>
2023-01-25 14:45:32 +0100

    update single template job_listing

--- public/partials/job_listing/custom_view/index.php
+++ public/partials/job_listing/custom_view/index.php
@@ -1,6 +1,6 @@
 <?php
 global $post;
-var_dump(get_post_custom($post->ID),$post);die;
+//var_dump(get_post_custom($post->ID),$post);die;
 get_header();
 
 ?>
@@ -8,20 +8,79 @@ get_header();
 <div class="container">
 
     <div class="entry-content row">
-        <h2 class="entry-title-2">PRESENTATION</h2>  
+        <h2 class="entry-title-2">PRESENTATION</h2> 
     </div>
     
     <div class="entry-content row">
-        <div class="entry-content col-7">
+        <div class="col-md-7">
             <div class="entry-content row">
-                <h2 class="entry-title-2"><?php echo $post->post_title ?></h2>
+               <h3> <b> Fiche identitaire </b> </h3>
             </div>
             <div class="entry-content row">
-                <h4 class="entry-title-2">Demandeur <?php echo get_post_custom($post->ID)['demandeur_emplois'][0] ?></h4>
+                <h4 class="entry-title-2">Type de formation : <?php echo get_post_custom($post->ID)['_ecole_type_formation'][0] ?></h4>
+            </div>
+            <div class="entry-content row">
+                <h4 class="entry-title-2">Statut : <?php echo get_post_custom($post->ID)['_ecole_statut'][0] ?></h4>
+            </div>
+            <div class="entry-content row">
+                <h4 class="entry-title-2">Type de structure : <?php echo get_post_custom($post->ID)['_ecole_type_structure'][0] ?></h4>
+            </div>
+            <div class="entry-content row">
+                <h4 class="entry-title-2">Année de création : <?php echo get_post_custom($post->ID)['_ecole_annee_creation'][0] ?></h4>
+            </div>
+            <div class="entry-content row">
+                <h4 class="entry-title-2">Habilitation à délivrer le doctorat : <?php echo get_post_custom($post->ID)['_ecole_habilitation_delivrer_doctorat'][0] ?></h4>
+            </div>
+            <div class="entry-content row">
+                <h4 class="entry-title-2">Prépa intégrée : <?php echo get_post_custom($post->ID)['_ecole_prepa_integree'][0] ?></h4>
+            </div>
+            <div class="entry-content row">
+                <h4 class="entry-title-2">Type d'habilitation : <?php echo get_post_custom($post->ID)['_ecole_type_habilitation'][0] ?></h4>
+            </div>
+
+            <br>
+
+            <div class="entry-content row">
+               <h3><b> Contacts </b></h3>
+            </div>
+             <div class="entry-content row">
+                <h4 class="entry-title-2">Directeur général : <?php echo get_post_custom($post->ID)['_ecole_dg_civilite'][0] ?> <?php echo get_post_custom($post->ID)['_ecole_dg_nom'][0] ?></h4>
+            </div>
+            <div class="entry-content row">
+                <h4 class="entry-title-2">Responsable des formations : <?php echo get_post_custom($post->ID)['_ecole_resp_formation'][0] ?></h4>
+            </div>
+            <div class="entry-content row">
+                <h4 class="entry-title-2">Adresse : <?php echo get_post_custom($post->ID)['geolocation_formatted_address'][0] ?></h4>
+            </div>
+
+            <br>
+            <br>
+            <br>
+            
+            <div class="entry-content row">
+               <h3> <b>  <?php echo $post->post_title ?> </b> </h3>
+            </div>
+            <div class="entry-content row">
+               <h5> <?php echo get_post_custom($post->ID)['_ecole_nom'][0] ?> </h5>
+            </div>
+
+            <br>
+
+            <div class="entry-content row">
+               <h3><b> Contacts </b></h3>
+            </div>
+             <div class="entry-content row">
+                <h4 class="entry-title-2">Directeur général : <?php echo get_post_custom($post->ID)['_ecole_dg_civilite'][0] ?> <?php echo get_post_custom($post->ID)['_ecole_dg_nom'][0] ?></h4>
+            </div>
+            <div class="entry-content row">
+                <h4 class="entry-title-2">Responsable des formations : <?php echo get_post_custom($post->ID)['_ecole_resp_formation'][0] ?></h4>
+            </div>
+            <div class="entry-content row">
+                <h4 class="entry-title-2">Adresse : <?php echo get_post_custom($post->ID)['geolocation_formatted_address'][0] ?></h4>
             </div>
         </div>
 
-        <div class="entry-content col-3">
+        <div class="col-md-3">
             <!-- map -->
         </div>
     </div>
